66e38af266c59e1ca9fa625434adecd78ede2ad5
zyr17 <jzjqz17@gmail.com>
2019-01-16 09:20:38 +0000

    update

--- cDCGAN.py
+++ cDCGAN.py
@@ -1,324 +1,252 @@
-import os, time
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import itertools
-import pickle
-import imageio
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
-from torch.autograd import Variable
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 15 14:59:08 2019
+
+@author: 13236
+"""
+
+from __future__ import absolute_import, division, print_function
+
+# Import TensorFlow >= 1.10 and enable eager execution
+import tensorflow as tf
+tf.enable_eager_execution()
+
+import os
+import time
 import numpy as np
+import glob
+import matplotlib.pyplot as plt
+import PIL
+import imageio
+from IPython import display
+from plot import plot
 
-typename = ['arm', 'baseball']
-#typename = ['airplane', 'ant', 'apple', 'bat', 'baseball', 'bee', 'bed', 'The Eiffel Tower', 'bicycle', 'axe']
+imagenames = ['circle', 'square']
 
-def loadquickdrawdata(number = 100):
-    filenames = typename
-    folder = 'data/quickdraw/'
-    resx = []
-    resy = []
-    for num, fname in enumerate(filenames):
-        fname = folder + fname + '.npy'
-        tmat = np.load(fname)[:number].reshape(-1, 1, 28, 28).astype('float') / 255 * 2 - 1
-        resx += list(tmat)
-        resy += [num] * len(tmat)
-    return resx, resy
+train_images = []
+train_labels = []
 
+for i in range(len(imagenames)):
+    timg = np.load('data/' + imagenames[i] + '.npy')
+    tlabel = [i] * len(timg)
+    train_images.append(timg)
+    train_labels += tlabel
 
-# G(z)
-class generator(nn.Module):
-    # initializers
-    def __init__(self, d=128):
-        super(generator, self).__init__()
-        self.deconv1_1 = nn.ConvTranspose2d(100, d*2, 4, 1, 0)
-        self.deconv1_1_bn = nn.BatchNorm2d(d*2)
-        self.deconv1_2 = nn.ConvTranspose2d(10, d*2, 4, 1, 0)
-        self.deconv1_2_bn = nn.BatchNorm2d(d*2)
-        self.deconv2 = nn.ConvTranspose2d(d*4, d*2, 4, 2, 1)
-        self.deconv2_bn = nn.BatchNorm2d(d*2)
-        self.deconv3 = nn.ConvTranspose2d(d*2, d, 4, 2, 1)
-        self.deconv3_bn = nn.BatchNorm2d(d)
-        self.deconv4 = nn.ConvTranspose2d(d, 1, 4, 2, 3)
+train_images = np.vstack(train_images)
+train_labels = np.array(train_labels)
 
-    # weight_init
-    def weight_init(self, mean, std):
-        for m in self._modules:
-            normal_init(self._modules[m], mean, std)
+#train_images = np.load('data/' + 'cloud.npy')
+num_total = train_images.shape[0]
+train_images = train_images.reshape((num_total,28,28))
 
-    # forward method
-    def forward(self, input, label):
-        x = F.relu(self.deconv1_1_bn(self.deconv1_1(input)))
-        y = F.relu(self.deconv1_2_bn(self.deconv1_2(label)))
-        x = torch.cat([x, y], 1)
-        x = F.relu(self.deconv2_bn(self.deconv2(x)))
-        x = F.relu(self.deconv3_bn(self.deconv3(x)))
-        x = F.tanh(self.deconv4(x))
-        # x = F.relu(self.deconv4_bn(self.deconv4(x)))
-        # x = F.tanh(self.deconv5(x))
+#(train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
 
-        return x
+train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+# We are normalizing the images to the range of [-1, 1]
+train_images = (train_images - 127.5) / 127.5
 
-class discriminator(nn.Module):
-    # initializers
-    def __init__(self, d=128):
-        super(discriminator, self).__init__()
-        self.conv1_1 = nn.Conv2d(1, d/2, 4, 2, 3)
-        self.conv1_2 = nn.Conv2d(10, d/2, 4, 2, 3)
-        self.conv2 = nn.Conv2d(d, d*2, 4, 2, 1)
-        self.conv2_bn = nn.BatchNorm2d(d*2)
-        self.conv3 = nn.Conv2d(d*2, d*4, 4, 2, 1)
-        self.conv3_bn = nn.BatchNorm2d(d*4)
-        self.conv4 = nn.Conv2d(d * 4, 1, 4, 1, 0)
+BUFFER_SIZE = len(train_images)
+BATCH_SIZE = 256
 
-    # weight_init
-    def weight_init(self, mean, std):
-        for m in self._modules:
-            normal_init(self._modules[m], mean, std)
+train_dataset = tf.data.Dataset.from_tensor_slices({'img': train_images, 'label': train_labels}).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
 
-    # forward method
-    def forward(self, input, label):
-        #print(input.size(), label.size())
-        x = F.leaky_relu(self.conv1_1(input), 0.2)
-        y = F.leaky_relu(self.conv1_2(label), 0.2)
-        #print(x.size(), y.size())
-        x = torch.cat([x, y], 1)
-        x = F.leaky_relu(self.conv2_bn(self.conv2(x)), 0.2)
-        x = F.leaky_relu(self.conv3_bn(self.conv3(x)), 0.2)
-        x = F.sigmoid(self.conv4(x))
+class Generator(tf.keras.Model):
+  def __init__(self):
+    super(Generator, self).__init__()
+    self.fc1 = tf.keras.layers.Dense(7*7*64, use_bias=False)
+    self.batchnorm1 = tf.keras.layers.BatchNormalization()
+    
+    self.conv1 = tf.keras.layers.Conv2DTranspose(64, (5, 5), strides=(1, 1), padding='same', use_bias=False)
+    self.batchnorm2 = tf.keras.layers.BatchNormalization()
+    
+    self.conv2 = tf.keras.layers.Conv2DTranspose(32, (5, 5), strides=(2, 2), padding='same', use_bias=False)
+    self.batchnorm3 = tf.keras.layers.BatchNormalization()
+    
+    self.conv3 = tf.keras.layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False)
 
-        return x
+  def call(self, x, training=True):
+    x = self.fc1(x)
+    x = self.batchnorm1(x, training=training)
+    x = tf.nn.relu(x)
 
-def normal_init(m, mean, std):
-    if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
-        m.weight.data.normal_(mean, std)
-        m.bias.data.zero_()
+    x = tf.reshape(x, shape=(-1, 7, 7, 64))
 
-# fixed noise & label
-temp_z_ = torch.randn(10, 100)
-fixed_z_ = temp_z_
-fixed_y_ = torch.zeros(10, 1)
-for i in range(9):
-    fixed_z_ = torch.cat([fixed_z_, temp_z_], 0)
-    temp = torch.ones(10, 1) + i
-    fixed_y_ = torch.cat([fixed_y_, temp], 0)
+    x = self.conv1(x)
+    x = self.batchnorm2(x, training=training)
+    x = tf.nn.relu(x)
 
-fixed_z_ = fixed_z_.view(-1, 100, 1, 1)
-fixed_y_label_ = torch.zeros(100, 10)
-fixed_y_label_.scatter_(1, fixed_y_.type(torch.LongTensor), 1)
-fixed_y_label_ = fixed_y_label_.view(-1, 10, 1, 1)
-fixed_z_, fixed_y_label_ = Variable(fixed_z_.cuda(), volatile=True), Variable(fixed_y_label_.cuda(), volatile=True)
-def show_result(num_epoch, show = False, save = False, path = 'result.png'):
+    x = self.conv2(x)
+    x = self.batchnorm3(x, training=training)
+    x = tf.nn.relu(x)
 
-    G.eval()
-    test_images = G(fixed_z_, fixed_y_label_)
-    G.train()
+    x = tf.nn.tanh(self.conv3(x))  
+    return x
 
-    size_figure_grid = 10
-    fig, ax = plt.subplots(size_figure_grid, size_figure_grid, figsize=(5, 5))
-    for i, j in itertools.product(range(size_figure_grid), range(size_figure_grid)):
-        ax[i, j].get_xaxis().set_visible(False)
-        ax[i, j].get_yaxis().set_visible(False)
+class Discriminator(tf.keras.Model):
+  def __init__(self):
+    super(Discriminator, self).__init__()
+    self.conv1 = tf.keras.layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same')
+    self.conv2 = tf.keras.layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same')
+    self.dropout = tf.keras.layers.Dropout(0.3)
+    self.flatten = tf.keras.layers.Flatten()
+    self.fc1 = tf.keras.layers.Dense(1)
 
-    for k in range(10*10):
-        i = k // 10
-        j = k % 10
-        ax[i, j].cla()
-        ax[i, j].imshow(test_images[k, 0].cpu().data.numpy(), cmap='gray')
+  def call(self, x, training=True):
+    x = tf.nn.leaky_relu(self.conv1(x))
+    x = self.dropout(x, training=training)
+    x = tf.nn.leaky_relu(self.conv2(x))
+    x = self.dropout(x, training=training)
+    x = self.flatten(x)
+    x = self.fc1(x)
+    return x
 
-    label = 'Epoch {0}'.format(num_epoch)
-    fig.text(0.5, 0.04, label, ha='center')
-    plt.savefig(path)
+generator = Generator()
+discriminator = Discriminator()
 
-    if show:
-        plt.show()
+# Defun gives 10 secs/epoch performance boost
+generator.call = tf.contrib.eager.defun(generator.call)
+discriminator.call = tf.contrib.eager.defun(discriminator.call)
+
+def discriminator_loss(real_output, generated_output):
+    # [1,1,...,1] with real output since it is true and we want
+    # our generated examples to look like it
+    real_loss = tf.losses.sigmoid_cross_entropy(multi_class_labels=tf.ones_like(real_output), logits=real_output)
+    #print(real_output)
+    #print(real_output.shape)
+    #raise Exception('pass')
+    
+    # [0,0,...,0] with generated images since they are fake
+    generated_loss = tf.losses.sigmoid_cross_entropy(multi_class_labels=tf.zeros_like(generated_output), logits=generated_output)
+
+    total_loss = real_loss + generated_loss
+
+    return total_loss
+
+def generator_loss(generated_output):
+    return tf.losses.sigmoid_cross_entropy(tf.ones_like(generated_output), generated_output)
+
+discriminator_optimizer = tf.train.AdamOptimizer(1e-4)
+generator_optimizer = tf.train.AdamOptimizer(1e-4)
+
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                 discriminator_optimizer=discriminator_optimizer,
+                                 generator=generator,
+                                 discriminator=discriminator)
+
+EPOCHS = 150000
+noise_dim = 100
+num_examples_to_generate = 16
+
+# keeping the random vector constant for generation (prediction) so
+# it will be easier to see the improvement of the gan.
+random_vector_for_generation = tf.random_normal([num_examples_to_generate,
+                                                 noise_dim])
+
+def generate_and_save_images(model, epoch, test_input):
+  # make sure the training parameter is set to False because we
+  # don't want to train the batchnorm layer when doing inference.
+  predictions = model(test_input, training=False)
+
+  fig = plt.figure(figsize=(4,4))
+  
+  for i in range(predictions.shape[0]):
+      plt.subplot(4, 4, i+1)
+      plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+      plt.axis('off')
+        
+  plt.savefig('imgs/image_at_epoch_{:04d}.png'.format(epoch))
+  plt.show()
+   
+def train(dataset, epochs, noise_dim):  
+    
+  gen_loss_record, disc_loss_record = [], []
+  for epoch in range(epochs):
+    start = time.time()
+    
+    index = 0
+    for onedata in dataset:
+      images = onedata['img']
+      labels = onedata['label']
+      '''
+      print(images.shape, labels.shape)
+      for i in range(5):
+        print(labels[i])
+      '''
+    
+      # generating noise from a uniform distribution
+      noise = tf.random_normal([BATCH_SIZE, noise_dim])
+      
+      with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        generated_images = generator(noise, training=True)
+      
+        real_output = discriminator(images, training=True)
+        generated_output = discriminator(generated_images, training=True)
+        
+        gen_loss = generator_loss(generated_output)
+        disc_loss = discriminator_loss(real_output, generated_output)
+        
+        gen_loss_record.append(gen_loss.numpy())
+        disc_loss_record.append(disc_loss.numpy())
+        
+      gradients_of_generator = gen_tape.gradient(gen_loss, generator.variables)
+      gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.variables)
+      
+      generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.variables))
+      discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.variables))
+      
+      index += 1
+      if index%20 == 0: 
+          print('epoch={}, gen_loss={}, disc_loss={}'.format(epoch, gen_loss, disc_loss))
+      
+    if epoch:
+      #display.clear_output(wait=True)
+      plot(epoch, gen_loss_record, 'gen_loss_record_{}'.format(epoch))
+      plot(epoch, disc_loss_record, 'disc_loss_record_{}'.format(epoch))
+      generate_and_save_images(generator,
+                               epoch + 1,
+                               random_vector_for_generation)
+      
+    
+    # saving (checkpoint) the model every 15 epochs
+    if (epoch + 1) % 15 == 0:
+      checkpoint.save(file_prefix = checkpoint_prefix)
+    
+    print ('Time taken for epoch {} is {} sec'.format(epoch,
+                                                      time.time()-start))
+  # generating after the final epoch
+  display.clear_output(wait=True)
+  generate_and_save_images(generator,
+                           epochs,
+                           random_vector_for_generation)
+  
+train(train_dataset, EPOCHS, noise_dim)
+
+# restoring the latest checkpoint in checkpoint_dir
+checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
+
+def display_image(epoch_no):
+  return PIL.Image.open('image_at_epoch_{:04d}.png'.format(epoch_no))
+
+display_image(EPOCHS)
+
+with imageio.get_writer('dcgan.gif', mode='I') as writer:
+  filenames = glob.glob('image*.png')
+  filenames = sorted(filenames)
+  last = -1
+  for i,filename in enumerate(filenames):
+    frame = 2*(i**0.5)
+    if round(frame) > round(last):
+      last = frame
     else:
-        plt.close()
+      continue
+    image = imageio.imread(filename)
+    writer.append_data(image)
+  image = imageio.imread(filename)
+  writer.append_data(image)
+    
+# this is a hack to display the gif inside the notebook
+os.system('cp dcgan.gif dcgan.gif.png')
 
-def show_train_hist(hist, show = False, save = False, path = 'Train_hist.png'):
-    x = range(len(hist['D_losses']))
-
-    y1 = hist['D_losses']
-    y2 = hist['G_losses']
-
-    plt.plot(x, y1, label='D_loss')
-    plt.plot(x, y2, label='G_loss')
-
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-
-    plt.legend(loc=4)
-    plt.grid(True)
-    plt.tight_layout()
-
-    if save:
-        plt.savefig(path)
-
-    if show:
-        plt.show()
-    else:
-        plt.close()
-
-# training parameters
-batch_size = 128
-lr = 0.0002
-train_epoch = 20
-
-# data_loader
-img_size = 28
-transform = transforms.Compose([
-        transforms.Scale(img_size),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-])
-x, y = loadquickdrawdata(10000)
-train_loader = torch.utils.data.DataLoader(
-    torch.utils.data.TensorDataset(torch.tensor(x).float(), torch.tensor(y).long()),
-    batch_size=batch_size, shuffle=True)
-
-# network
-G = generator(128)
-D = discriminator(128)
-G.weight_init(mean=0.0, std=0.02)
-D.weight_init(mean=0.0, std=0.02)
-G.cuda()
-D.cuda()
-
-# Binary Cross Entropy loss
-BCE_loss = nn.BCELoss()
-
-# Adam optimizer
-G_optimizer = optim.Adam(G.parameters(), lr=lr, betas=(0.5, 0.999))
-D_optimizer = optim.Adam(D.parameters(), lr=lr / 5, betas=(0.5, 0.999))
-
-# results save folder
-root = 'test_results/'
-model = 'test_'
-if not os.path.isdir(root):
-    os.mkdir(root)
-if not os.path.isdir(root + 'Fixed_results'):
-    os.mkdir(root + 'Fixed_results')
-
-train_hist = {}
-train_hist['D_losses'] = []
-train_hist['G_losses'] = []
-train_hist['per_epoch_ptimes'] = []
-train_hist['total_ptime'] = []
-
-# label preprocess
-onehot = torch.zeros(10, 10)
-onehot = onehot.scatter_(1, torch.LongTensor([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]).view(10,1), 1).view(10, 10, 1, 1)
-fill = torch.zeros([10, 10, img_size, img_size])
-for i in range(10):
-    fill[i, i, :, :] = 1
-
-print('training start!')
-start_time = time.time()
-for epoch in range(train_epoch):
-    D_losses = []
-    G_losses = []
-
-    # learning rate decay
-    if (epoch+1) == 11:
-        G_optimizer.param_groups[0]['lr'] /= 10
-        D_optimizer.param_groups[0]['lr'] /= 10
-        print("learning rate change!")
-
-    if (epoch+1) == 16:
-        G_optimizer.param_groups[0]['lr'] /= 10
-        D_optimizer.param_groups[0]['lr'] /= 10
-        print("learning rate change!")
-
-    epoch_start_time = time.time()
-    y_real_ = torch.ones(batch_size)
-    y_fake_ = torch.zeros(batch_size)
-    y_real_, y_fake_ = Variable(y_real_.cuda()), Variable(y_fake_.cuda())
-    for x_, y_ in train_loader:
-        #print(x_.size(), y_.size())
-        #print(torch.min(x_), torch.max(x_))
-        # train discriminator D
-        D.zero_grad()
-
-        mini_batch = x_.size()[0]
-
-        if mini_batch != batch_size:
-            y_real_ = torch.ones(mini_batch)
-            y_fake_ = torch.zeros(mini_batch)
-            y_real_, y_fake_ = Variable(y_real_.cuda()), Variable(y_fake_.cuda())
-
-        y_fill_ = fill[y_]
-        x_, y_fill_ = Variable(x_.cuda()), Variable(y_fill_.cuda())
-
-        D_result = D(x_, y_fill_).squeeze()
-        D_real_loss = BCE_loss(D_result, y_real_)
-
-        z_ = torch.randn((mini_batch, 100)).view(-1, 100, 1, 1)
-        y_ = (torch.rand(mini_batch, 1) * 10).type(torch.LongTensor).squeeze()
-        y_label_ = onehot[y_]
-        y_fill_ = fill[y_]
-        z_, y_label_, y_fill_ = Variable(z_.cuda()), Variable(y_label_.cuda()), Variable(y_fill_.cuda())
-
-        G_result = G(z_, y_label_)
-        D_result = D(G_result, y_fill_).squeeze()
-
-        D_fake_loss = BCE_loss(D_result, y_fake_)
-        D_fake_score = D_result.data.mean()
-
-        D_train_loss = D_real_loss + D_fake_loss
-
-        D_train_loss.backward()
-        D_optimizer.step()
-
-        D_losses.append(D_train_loss.item())
-
-        # train generator G
-        G.zero_grad()
-
-        z_ = torch.randn((mini_batch, 100)).view(-1, 100, 1, 1)
-        y_ = (torch.rand(mini_batch, 1) * 10).type(torch.LongTensor).squeeze()
-        y_label_ = onehot[y_]
-        y_fill_ = fill[y_]
-        z_, y_label_, y_fill_ = Variable(z_.cuda()), Variable(y_label_.cuda()), Variable(y_fill_.cuda())
-
-        G_result = G(z_, y_label_)
-        D_result = D(G_result, y_fill_).squeeze()
-
-        G_train_loss = BCE_loss(D_result, y_real_)
-
-        G_train_loss.backward()
-        G_optimizer.step()
-
-        G_losses.append(G_train_loss.item())
-
-    epoch_end_time = time.time()
-    per_epoch_ptime = epoch_end_time - epoch_start_time
-
-    print('[%d/%d] - ptime: %.2f, loss_d: %.3f, loss_g: %.3f' % ((epoch + 1), train_epoch, per_epoch_ptime, torch.mean(torch.FloatTensor(D_losses)),
-                                                              torch.mean(torch.FloatTensor(G_losses))))
-    fixed_p = root + 'Fixed_results/' + model + str(epoch + 1) + '.png'
-    show_result((epoch+1), save=True, path=fixed_p)
-    train_hist['D_losses'].append(torch.mean(torch.FloatTensor(D_losses)))
-    train_hist['G_losses'].append(torch.mean(torch.FloatTensor(G_losses)))
-    train_hist['per_epoch_ptimes'].append(per_epoch_ptime)
-
-end_time = time.time()
-total_ptime = end_time - start_time
-train_hist['total_ptime'].append(total_ptime)
-
-print("Avg one epoch ptime: %.2f, total %d epochs ptime: %.2f" % (torch.mean(torch.FloatTensor(train_hist['per_epoch_ptimes'])), train_epoch, total_ptime))
-print("Training finish!... save training results")
-torch.save(G.state_dict(), root + model + 'generator_param.pkl')
-torch.save(D.state_dict(), root + model + 'discriminator_param.pkl')
-with open(root + model + 'train_hist.pkl', 'wb') as f:
-    pickle.dump(train_hist, f)
-
-show_train_hist(train_hist, save=True, path=root + model + 'train_hist.png')
-
-images = []
-for e in range(train_epoch):
-    img_name = root + 'Fixed_results/' + model + str(e + 1) + '.png'
-    images.append(imageio.imread(img_name))
-imageio.mimsave(root + model + 'generation_animation.gif', images, fps=5)
+display.Image(filename="dcgan.gif.png")
